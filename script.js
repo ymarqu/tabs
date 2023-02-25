d48d48d2e0a4b0b77242069bb75af9d4a3b987ec
ymarquina <yeseniamrqna@gmail.com>
2023-02-25 10:29:53 -0800

    Fix JS functions and syntax

--- script.js
+++ script.js
@@ -1,38 +1,26 @@
 const textConatiner = document.querySelector(".desc")
 let btns = document.querySelectorAll(".btn");
 let cards = document.querySelectorAll(".card-text");
-let cardTitle = document.querySelectorAll(".card-title")
 let curYear = document.querySelector(".year");
-let info = [
-    {
-        tile: "",
-        content: ""
-    },
-    {
-        title: "Experience",
-        content: ["", "","", "", ""]
-    }
-]
+
 
 document.addEventListener("DOMContentLoaded", () =>{
     let year = new Date().getFullYear();
     curYear.innerHTML = year;
-
-
-})
-
-
+});
 
 
 btns.forEach(btn => {
-    btn.addEventListener('click', (e)=>{
-        for(let i = 0; i < cardTitle.length; i++){
-            cards[i].classList.remove("active");
-            btns[i].classList.remove("color");
-            if(btn.dataset.id == cardTitle[i].innerHTML){
-                cardTitle[i].parentNode.classList.add("active")
-                btn.classList.add("color");
-            }
-        }
+    btn.addEventListener('click', () => {
+        btns.forEach(b => {
+            b.classList.remove("color");
+            btn.classList.add("color");
+        })
+        let id = btn.dataset.id;
+    cards.forEach(card => {
+        card.classList.remove("active");
     })
-})
+    let container = document.getElementById(id);
+    container.classList.add("active");
+    });
+});
